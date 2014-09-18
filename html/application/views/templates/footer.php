5488b8d55bd4566d30fb6791437a9737532efb42
Al Longyear <al.longyear@gmail.com>
2014-09-17 21:34:12 -0700

    Added footer and header templates

---
+++ html/application/views/templates/footer.php
@@ -0,0 +1,7 @@
+		<br/>
+		<hr/>
+		<strong>Copyright &copy; 2014 My Content Copyright</strong>
+		<br/>
+		<small>portions provided by CodeIgniter and ion_auth under license</small>
+	</body>
+</html>
